2bf9cb854eb7027a002c5196c2b831898a3d1f7a
zethodderskov <zethodderskov@gmail.com>
2020-07-08 16:24:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Logical ACF
+
+A package that makes it possible to make and maintain ACF-fields from the source code.
+
---
+++ src/BlockGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LogicalAcf;
+
+class BlockGenerator {
+
+	public function __construct(){
+		echo 'Test';
+	}
+}
+?>
